91fc5646990deb29e1cc3d70257531d1c7675c94
Juanita Blignaut <576759@student.belgiumcampus.ac.za>
2022-08-28 21:00:33 +0200

    created repository

---
+++ src/main/java/com/example/Blignaut_Juanita_PRG381_Project/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.Blignaut_Juanita_PRG381_Project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+    
+    
+}
