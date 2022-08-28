bf60464e8de1a357ac2b2725f73498b75387ed1d
Juanita Blignaut <576759@student.belgiumcampus.ac.za>
2022-08-28 20:02:46 +0200

    first commit

---
+++ src/test/java/com/example/Blignaut_Juanita_PRG381_Project/BlignautJuanitaPrg381ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blignaut_Juanita_PRG381_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlignautJuanitaPrg381ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
