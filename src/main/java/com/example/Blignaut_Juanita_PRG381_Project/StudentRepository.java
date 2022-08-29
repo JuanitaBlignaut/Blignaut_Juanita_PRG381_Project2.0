1185a8eff3befe5511855aa786c616b793373d03
Juanita Blignaut <576759@student.belgiumcampus.ac.za>
2022-08-29 16:27:41 +0200

    added query

--- src/main/java/com/example/Blignaut_Juanita_PRG381_Project/StudentRepository.java
+++ src/main/java/com/example/Blignaut_Juanita_PRG381_Project/StudentRepository.java
@@ -1,8 +1,11 @@
 package com.example.Blignaut_Juanita_PRG381_Project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
     
+    @Query("SELECT u FROM Student u WHERE u.email = ?1")
+    public Student findByEmail(String email);
     
 }
