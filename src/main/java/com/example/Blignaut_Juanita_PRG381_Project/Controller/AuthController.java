8ce9e182650ef5cdf5dc34053ea1592185a138a1
Juanita Blignaut <576759@student.belgiumcampus.ac.za>
2022-08-29 16:28:05 +0200

    added authentiaction controller

---
+++ src/main/java/com/example/Blignaut_Juanita_PRG381_Project/Controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.example.Blignaut_Juanita_PRG381_Project.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Blignaut_Juanita_PRG381_Project.Student;
+import com.example.Blignaut_Juanita_PRG381_Project.StudentRepository;
+
+@Controller
+public class AuthController {
+    
+    @Autowired
+    private StudentRepository studentRepo;
+
+    @GetMapping("/index")
+    public String viewHomePage(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("student", new Student());
+         
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String processRegister(Student student) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(student.getPassword());
+        student.setPassword(encodedPassword);
+        
+        studentRepo.save(student);
+        
+        return "register_success";
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model mod){
+        List<Student> listStudents = studentRepo.findAll();
+        mod.addAttribute("listStudents", listStudents);
+
+        return "students";
+        
+    }
+}
