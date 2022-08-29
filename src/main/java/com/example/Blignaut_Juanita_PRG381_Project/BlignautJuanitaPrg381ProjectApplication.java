19415168575de3e5217229ce2aaf43e513b57e14
Juanita Blignaut <576759@student.belgiumcampus.ac.za>
2022-08-29 16:30:49 +0200

    changed layout

--- src/main/java/com/example/Blignaut_Juanita_PRG381_Project/BlignautJuanitaPrg381ProjectApplication.java
+++ src/main/java/com/example/Blignaut_Juanita_PRG381_Project/BlignautJuanitaPrg381ProjectApplication.java
@@ -3,11 +3,13 @@ package com.example.Blignaut_Juanita_PRG381_Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BlignautJuanitaPrg381ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlignautJuanitaPrg381ProjectApplication.class, args);
+		
 	}
 
 }
